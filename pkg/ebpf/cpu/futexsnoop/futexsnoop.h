ef9a02883afed692709f90211e5a68f974d16e81
Tao Chen <chen.dylane@gmail.com>
2024-04-08 23:29:50 +0800

    futexsnoop: Fix c code style
    Signed-off-by: Tao Chen <chen.dylane@gmail.com>

--- pkg/ebpf/cpu/futexsnoop/futexsnoop.h
+++ pkg/ebpf/cpu/futexsnoop/futexsnoop.h
@@ -5,8 +5,8 @@
 #ifndef __FUTEXCTN_H
 #define __FUTEXCTN_H
 
-#define TASK_COMM_LEN   16
-#define MAX_SLOTS       24
+#define TASK_COMM_LEN 16
+#define MAX_SLOTS 24
 
 typedef signed char __s8;
 typedef unsigned char __u8;
@@ -26,46 +26,46 @@ typedef __s64 s64;
 typedef __u64 u64;
 
 struct hist_key {
-        __u64 pid_tgid;
-        __u64 uaddr;
-        int user_stack_id;
+	__u64 pid_tgid;
+	__u64 uaddr;
+	int user_stack_id;
 };
 
 struct user_args {
-        __u32 targ_pid;
-        __u32 targ_tid;
+	__u32 targ_pid;
+	__u32 targ_tid;
 	/* lock addr */
-        __u64 targ_lock;
-        bool stack;
-        __u32 min_dur_ms;
-        __u32 max_dur_ms;
+	__u64 targ_lock;
+	bool stack;
+	__u32 min_dur_ms;
+	__u32 max_dur_ms;
 	/* user count hold the same lock */
-        __u32 max_lock_hold_users;
+	__u32 max_lock_hold_users;
 };
 // for single task
 struct hist {
-        __u32 slots[MAX_SLOTS];
-        char comm[TASK_COMM_LEN];
-        __u64 pid_tgid;
-        __u64 uaddr;
-        int user_stack_id;
-        __u32 pad;
-        __u64 contended;
-        __u64 total_elapsed;
-        __u64 min_dur;
-        __u64 max_dur;
-        __u64 delta_dur;
-        __u64 max_dur_ts;
+	__u32 slots[MAX_SLOTS];
+	char comm[TASK_COMM_LEN];
+	__u64 pid_tgid;
+	__u64 uaddr;
+	int user_stack_id;
+	__u32 pad;
+	__u64 contended;
+	__u64 total_elapsed;
+	__u64 min_dur;
+	__u64 max_dur;
+	__u64 delta_dur;
+	__u64 max_dur_ts;
 };
 
 // for single lock
 struct lock_stat {
-        uint32_t user_cnt;
-        uint32_t max_user_cnt;
-        __u64 uaddr;
-        char comm[TASK_COMM_LEN];
-        __u64 pid_tgid;
-        __u64 ts;
+	uint32_t user_cnt;
+	uint32_t max_user_cnt;
+	__u64 uaddr;
+	char comm[TASK_COMM_LEN];
+	__u64 pid_tgid;
+	__u64 ts;
 };
 
 #endif /* FUTEXCTN_H_ */
